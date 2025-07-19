a1d57851ada3e362641ca9cee28dfffbc242aeae
Prashant-Jain-Hiteshi <prashant.jain@hiteshi.com>
2025-07-19 19:04:56 +0530

    Implemented the header UI

--- src/components/pageLayout/Header.tsx
+++ src/components/pageLayout/Header.tsx
@@ -6,14 +6,16 @@ import Hamburger from "../../assets/Humburger.png";
 
 const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
   return (
-    <div className="flex  items-center justify-between  text-black bg-green-50   ">
+    <div className="flex  items-center justify-end  text-white bg-card p-4">
       <div className="md:hidden  h-fit pl-3">
         <button onClick={toggleSidebar} className="">
           <Image src={Hamburger} alt="hamburger-icon" className="w-7" />
         </button>
       </div>
       <div className="flex-1 md:hidden" />
-      <div className="ml-auto  ">gfg</div>
+      <div className="">
+        <p>Profile</p>
+      </div>
     </div>
   );
 };
